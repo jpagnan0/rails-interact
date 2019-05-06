8442d7e87b07142e35c2a28603abd8871aa1b063
jpagnan0 <a.jpagnano@gmail.com>
2019-05-06 10:45:22 -0400

    currently stable, TODO: fix error handling

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
       get '/current_medications', to: 'users#current_user_medications'
       get '/current_interactions', to: 'users#current_user_interactions'
       # get '/user/:id', to: 'users#current_user_medications'
-      get '/user_interactions/:id', to: 'interactions#user_interactions'
-      # post '/login', to: 'auth#create'
-      # get '/current_user', to: 'auth#get_current_user'
+      # get '/user_interactions/:id', to: 'interactions#user_interactions'
+      # post '/login', to: 'users#create'
+      get '/current_user', to: 'auth#get_current_user'
       # get '/dashboard', to: 'users#dashboard'
     end
   end
